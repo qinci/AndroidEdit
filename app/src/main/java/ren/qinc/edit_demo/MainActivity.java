89ed26fcea3bc501994d8623570a271641f399ed
qinci <946736079@qq.com>
2016-06-30 12:21:09 +0800

    提交代码

---
+++ app/src/main/java/ren/qinc/edit_demo/MainActivity.java
@@ -0,0 +1,49 @@
+package ren.qinc.edit_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import ren.qinc.edit.PerformEdit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    private PerformEdit mPerformEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = ((EditText) findViewById(R.id.editText));
+        mPerformEdit = new PerformEdit(mEditText);
+
+        mPerformEdit.setDefaultText("这是初始值,不能撤销的值");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId ==R.id.action_undo){
+            mPerformEdit.undo();
+            return true;
+        }else if(itemId == R.id.action_redo){
+            mPerformEdit.redo();
+            return true;
+        }else if(itemId == R.id.action_clear){
+            mPerformEdit.clearHistory();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ lib/src/main/java/ren/qinc/edit/PerformEdit.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2016. SHENQINCI(沈钦赐)<946736079@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ren.qinc.edit;
+
+import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.Stack;
+
+/**
+ * 撤销和恢复撤销
+ * Created by 沈钦赐 on 16/6/23.
+ */
+public class PerformEdit {
+    //操作序号(一次编辑可能对应多个操作，如替换文字，就是删除+插入)
+    int index;
+    Stack<Action> history = new Stack<>();
+    Stack<Action> historyBack = new Stack<>();
+
+    private Editable editable;
+    //撤销和重做操作标志，防止撤销时候，重复操作
+    private boolean flag = false;
+
+    public PerformEdit(@NonNull EditText editText) {
+        CheckNull(editText, "EditText不能为空");
+        editable = editText.getText();
+        editText.addTextChangedListener(new Watcher());
+    }
+
+    protected void onEditableChanged(Editable s) {
+
+    }
+
+    protected void onTextChanged(Editable s) {
+
+    }
+
+    /**
+     * 首次设置文本
+     * Set default text.
+     */
+    public final void setDefaultText(CharSequence text){
+        clearHistory();
+        flag = true;
+        editable.replace(0,editable.length(),text);
+        flag = false;
+    }
+
+    /**
+     * 清理记录
+     * Clear history.
+     */
+    public final void clearHistory() {
+        history.clear();
+        historyBack.clear();
+    }
+
+
+    /**
+     * 撤销
+     * Undo.
+     */
+    public final void undo() {
+        if (history.empty()) return;
+        //锁定操作
+        flag = true;
+        Action action = history.pop();
+        historyBack.push(action);
+        if (action.isAdd) {
+            //撤销添加
+            editable.delete(action.cursor, action.cursor + action.actionTarget.length());
+        } else {
+            //插销删除
+            editable.insert(action.cursor, action.actionTarget);
+        }
+        //释放操作
+        flag = false;
+        //判断是否是下一个动作是否和本动作是同一个操作，直到不同为止
+        if (!history.empty() && history.peek().index == action.index){
+            undo();
+        }
+    }
+
+    /**
+     * 恢复
+     * Redo.
+     */
+    public final void redo() {
+        if (historyBack.empty()) return;
+        flag = true;
+        Action action = historyBack.pop();
+        history.push(action);
+        if (action.isAdd)//恢复添加
+            editable.insert(action.cursor, action.actionTarget);
+        else//恢复删除
+            editable.delete(action.cursor, action.cursor + action.actionTarget.length());
+        flag = false;
+        //判断是否是下一个动作是否和本动作是同一个操作
+        if (!historyBack.empty() && historyBack.peek().index == action.index)
+            redo();
+    }
+
+    private class Watcher implements TextWatcher {
+
+        /**
+         * Before text changed.
+         *
+         * @param s     the s
+         * @param start the start 起始光标
+         * @param count the count 选择数量
+         * @param after the after 替换增加的文字数
+         */
+        @Override
+        public final void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            if (flag) return;
+            int end = start + count;
+            if (end > start && end <= s.length()) {
+                CharSequence charSequence = s.subSequence(start, end);
+                if (charSequence.length() > 0) {
+                    Log.i("test", "beforeTextChanged:" + charSequence + " start" + start + " count" + count + " after" + after);
+                    Action action = new Action(charSequence, start, false);
+                    history.push(action);
+                    historyBack.clear();
+                    action.setIndex(++index);
+                }
+            }
+        }
+
+        /**
+         * On text changed.
+         *
+         * @param s      the s
+         * @param start  the start 起始光标
+         * @param before the before 选择数量
+         * @param count  the count 添加的数量
+         */
+        @Override
+        public final void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (flag) return;
+            int end = start + count;
+            if (end > start) {
+                CharSequence charSequence = s.subSequence(start, end);
+                if (charSequence.length() > 0) {
+                    Log.i("test", "onTextChanged:" + charSequence + " start" + start + " before" + before + " count" + count);
+                    Action action = new Action(charSequence, start, true);
+                    history.push(action);
+                    historyBack.clear();
+                    if (before > 0) {
+                        //文字替换（先删除再增加），删除和增加是同一个操作，所以不需要增加序号
+                        action.setIndex(index);
+                    } else {
+                        action.setIndex(++index);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public final void afterTextChanged(Editable s) {
+            if (s != editable) {
+                editable = s;
+                onEditableChanged(s);
+            }
+            PerformEdit.this.onTextChanged(s);
+        }
+
+    }
+
+    private class Action {
+        /** 改变字符. */
+        CharSequence actionTarget;
+        /** 光标位置. */
+        int cursor;
+        /** 标志增加操作. */
+        boolean isAdd;
+        /** 操作序号. */
+        int index;
+
+        public Action(CharSequence actionTag, int cursor, boolean add) {
+            this.actionTarget = actionTag;
+            this.cursor = cursor;
+            this.isAdd = add;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+
+
+    private static void CheckNull(Object o,String message) {
+        if(o == null)throw new IllegalStateException(message);
+    }
+}
